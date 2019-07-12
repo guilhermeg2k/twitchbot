76264123c955983bbf895ead97e8ba471455aeaa
Guilherme Nascimento <guilhermen2k@gmail.com>
2019-07-12 05:35:00 -0300

    Added timed messages and words filter

--- bot.go
+++ bot.go
@@ -8,19 +8,51 @@ import (
 	"time"
 )
 
-//Bot twitch bot
+//Bot .
 type Bot struct {
-	Hello    string
-	Commands []map[string]*Command
+	Hello        string
+	BlockedMsg   string
+	Commands     []map[string]*Command
+	TimedMsgs    []map[string]*TimedMsg
+	BlockedWords []string
 }
 
-//Command user Command
+//TimedMsg .
+type TimedMsg struct {
+	Msg      string
+	Delay    uint
+	LastTime time.Time
+}
+
+//Command .
 type Command struct {
-	Awnser  string
+	Anwser  string
 	Delay   int
 	Nick    string
 	Msg     string
 	LasTime time.Time
+}
+
+func (twitch *Twitch) timedMessages() {
+	for {
+		for _, msg := range twitch.bot.TimedMsgs {
+			for _, key := range msg {
+				if time.Since(key.LastTime) > time.Second*time.Duration(key.Delay) {
+					key.LastTime = time.Now()
+					twitch.sendMessage(key.Msg)
+				}
+			}
+		}
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+}
+
+func (twitch *Twitch) wordFilter() {
+	for {
+		command := <-twitch.WordFilterChannel
+		twitch.timeOut(command.Nick, 1)
+		twitch.sendMessage(fmt.Sprintf("@%s %s", command.Nick, twitch.bot.BlockedMsg))
+	}
 }
 
 func (twitch *Twitch) handleCommand() {
@@ -28,17 +60,16 @@ func (twitch *Twitch) handleCommand() {
 		command := <-twitch.CommandsChannel
 		for _, _command := range twitch.bot.Commands {
 			if _, ok := _command[command.Msg]; ok {
-				//fmt.Println(twitch.bot)
 				if time.Since(_command[command.Msg].LasTime) > time.Second*time.Duration(_command[command.Msg].Delay) {
 					_command[command.Msg].LasTime = time.Now()
-					twitch.sendMenssage(fmt.Sprintf("@%s %s", command.Nick, _command[command.Msg].Awnser))
+					twitch.sendMessage(fmt.Sprintf("@%s %s", command.Nick, _command[command.Msg].Anwser))
 				}
 			}
 		}
 	}
 }
 
-//InicializeBot get a instance of struct Bot
+//InicializeBot .
 func InicializeBot() (bot Bot) {
 	botFile, err := os.Open("bot.json")
 	checkError(err)
@@ -47,4 +78,8 @@ func InicializeBot() (bot Bot) {
 	err = json.Unmarshal(fileData, &bot)
 	checkError(err)
 	return
+}
+
+func (twitch *Twitch) timeOut(nick string, time uint) {
+	twitch.sendMessage(fmt.Sprintf("/timeout %s %d", nick, time))
 }
--- socket.go
+++ socket.go
@@ -10,13 +10,12 @@ import (
 
 //Twitch connection
 type Twitch struct {
-	Conn            net.Conn
-	S               Settings
-	bot             Bot
-	CommandsChannel chan Command
+	Conn              net.Conn
+	S                 Settings
+	bot               Bot
+	CommandsChannel   chan Command
+	WordFilterChannel chan Command
 }
-
-var cliChannel chan string
 
 //SocketClient inicialize
 func SocketClient(s Settings) {
@@ -24,12 +23,12 @@ func SocketClient(s Settings) {
 	bot := InicializeBot()
 	addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
 	conn, err := net.Dial("tcp", addr)
-	twitch := Twitch{Conn: conn, S: s, bot: bot, CommandsChannel: make(chan Command)}
+	twitch := Twitch{Conn: conn, S: s, bot: bot, CommandsChannel: make(chan Command), WordFilterChannel: make(chan Command)}
 	checkError(err)
 	go twitch.handle()
 	twitch.authenticate()
 	twitch.joinChannel()
-	//twitch.sendMenssage(bot.Hello)
+	//twitch.sendMessage(bot.Hello)
 	stdIn := bufio.NewReader(os.Stdin)
 	for {
 		userInput, err = stdIn.ReadString('\n')
@@ -39,31 +38,45 @@ func SocketClient(s Settings) {
 }
 
 func (twitch *Twitch) handle() {
+	go twitch.wordFilter()
 	go twitch.handleCommand()
+	go twitch.timedMessages()
 	var err error
 	var buff []byte
 	var n int
 	var data string
+	var keepRunning bool
+	var msg Command
 	for {
 		buff = make([]byte, 1024)
 		n, err = twitch.Conn.Read(buff)
 		if err != nil {
 			checkError(err)
 		}
+		keepRunning = true
 		data = string(buff[0:n])
 		fmt.Println(data)
 		if strings.Contains(data, "PRIVMSG") {
-			msg := getCommandFromData(data)
-			for _, command := range twitch.bot.Commands {
-				if _, ok := command[msg.Msg]; ok {
-					fmt.Println(msg.Msg)
-					twitch.CommandsChannel <- msg
+			msg = getCommandFromData(data)
+			for _, blockedWord := range twitch.bot.BlockedWords {
+				if strings.Contains(data, blockedWord) {
+					twitch.WordFilterChannel <- msg
+					keepRunning = false
 					break
 				}
 			}
-			if strings.Contains(data, "PING") {
-				twitch.pong()
+			if keepRunning {
+				for _, command := range twitch.bot.Commands {
+					if _, ok := command[msg.Msg]; ok {
+						twitch.CommandsChannel <- msg
+						keepRunning = false
+						break
+					}
+				}
 			}
+		}
+		if strings.Contains(data, "PING") {
+			twitch.pong()
 		}
 	}
 }
@@ -80,7 +93,7 @@ func (twitch *Twitch) joinChannel() {
 	twitch.Conn.Write([]byte(fmt.Sprintf("JOIN #%s\r\n", twitch.S.Channel)))
 }
 
-func (twitch *Twitch) sendMenssage(msg string) {
+func (twitch *Twitch) sendMessage(msg string) {
 	twitch.Conn.Write([]byte(fmt.Sprintf("PRIVMSG #%s :%s\r\n", twitch.S.Channel, msg)))
 }
 
